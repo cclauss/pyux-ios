5b39b7c7aae77b6ea61d565959487f5efbd68466
sbbosco <73964492+sbbosco@users.noreply.github.com>
2026-06-26 10:30:23 -0600

    fix action_handler

--- ux/buttonitem.py
+++ ux/buttonitem.py
@@ -12,6 +12,7 @@ class ButtonItem():
     def __init__(self, title=None, image=None, action=None, menu=None, enabled=True, tint_color=None):
         self.action_handler = None
         if action:
+            self.action = action
             item_handler_block=Block(self._action, None, objc_id)
             item_action = UIAction.actionWithHandler_(item_handler_block)
         else:
